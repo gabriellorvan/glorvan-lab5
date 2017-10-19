5bfcb0544010a88817aadd71a001fdf440da212d
gabriellorvan <gabriel.lorvan1@marist.edu>
2017-10-19 07:11:53 -0400

    Maybe this time?

--- example.js
+++ example.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+res.sendFile('views/index.html', {root: __dirname })
 })
+
 
 app.listen(1337, function () {
   console.log('Example app listening on port 1337!')
